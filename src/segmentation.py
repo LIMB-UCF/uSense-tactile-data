9da0156d19a823aa3108d9c0d4374be265501f11
Zubaidah Almashhadani <zubaidahsabah@gmail.com>
2026-04-29 18:45:35 -0400

    Change save directory for segmented data output

--- src/segmentation.py
+++ src/segmentation.py
@@ -42,7 +42,7 @@ BASE_PATH = "/content/tactile"
 if __name__ == "__main__":
     textures_data = []
     labels = []  
-    save_dir = "/content/unary/segmented_data" 
+    save_dir = "/content/output/segmented_data"
     # os.makedirs(save_dir, exist_ok=True)
     save_path = os.path.join(save_dir, f"segmented_data.npz")
     if os.path.exists(save_path):
