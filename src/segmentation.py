6b622672c2faaa3b7af51c75e36431425153eb5d
Zubaidah Almashhadani <zubaidahsabah@gmail.com>
2026-04-29 18:59:19 -0400

    Fix folder naming to include zero-padding for texture

--- src/segmentation.py
+++ src/segmentation.py
@@ -57,8 +57,7 @@ if __name__ == "__main__":
         print(f"{texture} processing...")
         for speed in SPEEDS:
             for force in FORCES:
-               #folder = f'Texture0{texture.zfill(3)}
-                folder = f'Texture0{texture}'
+                folder = f'Texture0{texture.zfill(3)}
                 label = f'T{texture}_S{speed}_F{force}'
                 for trial in range(1, 101):
                     file = f'processed_S{speed}_F{force}_T{trial}.npz'
